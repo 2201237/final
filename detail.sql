ce82e49df6eaf725f6375e22d8c241dcc7bb0b79
2201237 <2201237@s.asojuku.ac.jp>
2024-01-12 10:34:34 +0900

    DB修正

--- detail.sql
+++ detail.sql
@@ -1,6 +1,5 @@
 CREATE TABLE Category (
-task_code INT(7) NOT NULL AUTO_INCREMENT,
-task_category VARCHAR(20),
-PRIMARY KEY(task_code),
-FOREIGN KEY (task_code) REFERENCES Task(task_code)
+category_code INT(7) NOT NULL AUTO_INCREMENT,
+category_name VARCHAR(20),
+PRIMARY KEY(category_code)
 );
